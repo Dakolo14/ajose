a51f9557ffac85925b82d2ecd4fafd487dc11fd8
Ajose Damilare <114034678+Dakolo14@users.noreply.github.com>
2023-11-04 08:17:27 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -160,7 +160,7 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.home__prefil, .about__image, .contact__mail`, {origin: 'right'})
-sr.reveal(`.home__name, .home__info, .about__container, .section__title-1, .about__info, .contact__social, .contact__data`, {origin: 'left'})
+sr.reveal(`.home__name, .home__info, .italics, .about__container, .section__title-1, .about__info, .contact__social, .contact__data`, {origin: 'left'})
 sr.reveal(`.services__card, .projects__card`, {interval: 100})
 
 
